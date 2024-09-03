6440b38079341a41fcebe12e3ed7eb91f96a453c
ghkdusghd <164862216+ghkdusghd@users.noreply.github.com>
2024-09-03 15:17:27 +0900

    ADD : jwt 및 oauth 관련 초기세팅 파일 추가

--- backend/src/main/java/kr/kh/backend/config/SecurityConfig.java
+++ backend/src/main/java/kr/kh/backend/config/SecurityConfig.java
@@ -3,16 +3,21 @@ package kr.kh.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-    @Configuration
-    public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http.cors();
-            return http.build();
-        }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf.disable());
+
+        return http.build();
     }
+
+}
 
 
